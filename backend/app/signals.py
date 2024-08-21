29c1c5b0517f6a5f53e2a343cf36c93d527c3f41
Cristhian <gfcristhian98@gmail.com>
2024-08-20 21:06:57 -0500

    feat: add real time sync

--- backend/app/signals.py
+++ backend/app/signals.py
@@ -1,21 +1,30 @@
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
-# from channels.layers import get_channel_layer
-# from asgiref.sync import async_to_sync
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 
-# from app.models import WorkflowRun
+from app.models import WorkflowRun
 
 
-# @receiver(post_save, sender=WorkflowRun)
-# def send_status_update(sender, instance, **kwargs):
-#     if "status" in instance.get_dirty_fields().keys():
-#         workspace_id = instance.resource.workspace_id
-#         channel_layer = get_channel_layer()
-#         async_to_sync(channel_layer.group_send)(
-#             f"workspace_{workspace_id}",
-#             {
-#                 "type": "workflow_status_update",
-#                 "status": instance.status,
-#                 "workflow_run_id": str(instance.id),
-#             },
-#         )
+@receiver(pre_save, sender=WorkflowRun)
+def send_status_update(sender, instance, **kwargs):
+    old_status = WorkflowRun.objects.get(pk=instance.pk).status
+    new_status = instance.status
+
+    if old_status == new_status:
+        return
+    workspace_id = instance.resource.workspace_id
+    channel_layer = get_channel_layer()
+    try:
+      async_to_sync(channel_layer.group_send)(
+          f"workspace_{workspace_id}",
+          {
+              "type": "workflow_status_update",
+              "status": instance.status,
+              "workflow_run_id": str(instance.id),
+              "resource_id": str(instance.resource.id),
+          },
+      )
+    except Exception as e:
+      print(e)
+      print("Error sending status update")
