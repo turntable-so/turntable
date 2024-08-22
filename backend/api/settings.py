5570bb8b24190bdd5d3b4339c85d0d31226d7d0e
Cristhian <gfcristhian98@gmail.com>
2024-08-22 16:19:46 -0500

    fix: fix login in docker

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-i7pd5iwtuo*0h9je%(n1!u8srlbka$^do)c(#y88h9grhilq3@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost", "api"]
 backend_host = os.getenv("BACKEND_HOST")
 if "https://" in backend_host:
     backend_host = backend_host.replace("https://", "")
