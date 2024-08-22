6ec1eb2a83e7522e051a8dff2c0183976cb13a5e
turntable-justin <122627527+turntable-justin@users.noreply.github.com>
2024-08-22 10:46:31 -0400

    fix: improve seed data script

--- backend/app/management/commands/seed_data.py
+++ backend/app/management/commands/seed_data.py
@@ -1,18 +1,79 @@
-import json
 import os
 
 from django.core.management.base import BaseCommand
 
 from app.models import (
-    BigqueryDetails,
     DBTCoreDetails,
-    GithubInstallation,
+    DBTResourceSubtype,
+    MetabaseDetails,
+    PostgresDetails,
     Resource,
     ResourceType,
     User,
     Workspace,
 )
 from vinyl.lib.dbt_methods import DBTVersion
+from workflows.metadata_sync import MetadataSyncWorkflow
+from workflows.utils.debug import WorkflowDebugger
+
+
+def local_user():
+    user = User.objects.create_user(
+        name="Turntable Dev", email="dev@turntable.so", password="mypassword"
+    )
+    return user
+
+
+def local_workspace(user):
+    # create turntable full
+    workspace = Workspace.objects.create(
+        id="org_2XVt0EheumDcoCerhQzcUlVmXvG",
+        name="Parkers Vinyl Shop",
+    )
+    workspace.add_admin(user)
+    workspace.save()
+    return workspace
+
+
+def local_postgres(workspace):
+    resource = Resource.objects.create(
+        workspace=workspace, name="Test Postgres Resource", type=ResourceType.DB
+    )
+    PostgresDetails(
+        resource=resource,
+        host=os.getenv("POSTGRES_TEST_DB_HOST", "postgres_test_db"),
+        port=os.getenv("POSTGRES_TEST_DB_PORT", 5432),
+        database="mydb",
+        username="myuser",
+        password="mypassword",
+    ).save()
+
+    DBTCoreDetails(
+        resource=resource,
+        project_path="fixtures/test_resources/jaffle_shop",
+        threads=1,
+        version=DBTVersion.V1_7.value,
+        subtype=DBTResourceSubtype.CORE,
+        database="mydb",
+        schema="dbt_sl_test",
+    ).save()
+
+    return resource
+
+
+def local_metabase(workspace):
+    resource = Resource.objects.create(
+        workspace=workspace, name="Test Metabase Resource", type=ResourceType.BI
+    )
+
+    MetabaseDetails(
+        resource=resource,
+        username="test@example.com",
+        password="mypassword1",
+        connect_uri=os.getenv("TEST_METABASE_URI", "http://metabase:4000"),
+    ).save()
+
+    return resource
 
 
 class Command(BaseCommand):
@@ -25,55 +86,11 @@ class Command(BaseCommand):
             )
             return
 
-        user = User.objects.create_user(
-            name="Turntable Dev", email="dev@turntable.so", password="mypassword"
-        )
-        # create turntable full
-        workspace = Workspace.objects.create(
-            id="org_2XVt0EheumDcoCerhQzcUlVmXvG",
-            name="Parkers Vinyl Shop",
-        )
-        # this bootstraps the necessary conditions
-        workspace.add_admin(user)
-        workspace.save()
-        github_installation = GithubInstallation.objects.create(
-            id=50270427,
-            workspace=workspace,
-            user=user,
-        )
-        github_installation2 = GithubInstallation.objects.create(
-            id=51092020, workspace=workspace, user=user
-        )
-        bigquery_resource = Resource.objects.create(
-            type=ResourceType.DB,
-            workspace=workspace,
-        )
-        if gac := os.getenv("GOOGLE_APPLICATION_CREDENTIALS"):
-            BigqueryDetails(
-                resource=bigquery_resource,
-                lookback_days=1,
-                schema_include=["analytics", "analytics-mini"],
-                service_account=json.loads(gac.replace("\n", "\\n")),
-            ).save()
-            DBTCoreDetails(
-                resource=bigquery_resource,
-                github_installation=github_installation,
-                github_repo_id="599220755",
-                project_path=".",
-                threads=6,
-                database="analytics-dev-372514",
-                version=DBTVersion.V1_5.value,
-                schema="analytics",
-            ).save()
-            DBTCoreDetails(
-                resource=bigquery_resource,
-                github_installation=github_installation2,
-                github_repo_id="804953758",
-                project_path=".",
-                threads=6,
-                database="analytics-dev-372514",
-                version=DBTVersion.V1_7.value,
-                schema="analytics_mini",
-            ).save()
+        user = local_user()
+        workspace = local_workspace(user)
+        postgres = local_postgres(workspace)
+        metabase = local_metabase(workspace)
+        WorkflowDebugger(MetadataSyncWorkflow, {"resource_id": postgres.id}).run()
+        WorkflowDebugger(MetadataSyncWorkflow, {"resource_id": metabase.id}).run()
 
         self.stdout.write(self.style.SUCCESS("Successfully seeded the database"))
