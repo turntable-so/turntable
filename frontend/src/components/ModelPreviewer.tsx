cb89ca98d9a4981455a1144793cae07beb06a2da
Haven <haven38@gmail.com>
2024-08-23 17:36:36 -0400

    Add ai-description logic to ModelPreviewer

--- frontend/src/components/ModelPreviewer.tsx
+++ frontend/src/components/ModelPreviewer.tsx
@@ -9,160 +9,221 @@ import { Separator } from "./ui/separator"
 import { Fragment, useEffect } from "react";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
 import { useAppContext } from "../contexts/AppContext";
+import Magic from "./icons/magic";
 
+function ColumnPreviewer({ column }: { column: any }) {
+  const columnDescription =
+    column.description || column.ai_description || "No description";
+  const isUsingAiDescription = !column.description && column.ai_description;
 
-export default function ModelPreviewer({ asset, context, clearAsset }: { context?: 'NOTEBOOK' | 'LINEAGE', asset: any, clearAsset: () => void }) {
+  return (
+    <>
+      <div className="p-4">
+        <div className="flex flex-col space-y-2">
+          <div className="font-bold font-mono">{column.name}</div>
+          <div className="font-mono font-medium text-gray-400 flex items-center text-xs">
+            <ColumnTypeIcon dataType={column.type} className="size-4 mr-1" />
+            {column.type}
+          </div>
+          <div className="font-normal grid gric- space-y-2 ml-[-8px]">
+            {(column.tests || []).map((test: string, i: number) => (
+              <Badge
+                variant="secondary"
+                className="w-fit text-muted-foreground"
+                key={i}
+              >
+                {test}
+              </Badge>
+            ))}
+          </div>
+          <div className="w-full grid grid-cols-10 items-center">
+            {isUsingAiDescription && (
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <Magic className="w-5 h-5 col-span-1" />
+                  </TooltipTrigger>
+                  <TooltipContent>AI Generated</TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            )}
+            <div className="col-span-9 pt-1 font-normal">
+              {columnDescription}
+            </div>
+          </div>
+        </div>
+      </div>
+      <Separator className="text-black" />
+    </>
+  );
+}
 
-    const { setInsertCurrentSqlContent } = useAppContext();
+export default function ModelPreviewer({
+  asset,
+  context,
+  clearAsset,
+}: {
+  context?: "NOTEBOOK" | "LINEAGE";
+  asset: any;
+  clearAsset: () => void;
+}) {
+  const { setInsertCurrentSqlContent } = useAppContext();
 
-    console.log({ asset })
+  const assetDescription =
+    asset.description || asset.ai_description || "No description";
+  const isUsingAiDescription = !asset.description && asset.ai_description;
 
+  const insertAssetSqlIntoCurrentContext = () => {
+    const sql = `SELECT\n  *\nFROM\n  ${asset.name}\nLIMIT 1000`;
+    setInsertCurrentSqlContent({
+      sql,
+      title: asset.name,
+      resourceId: asset.resource_id,
+    });
+  };
 
-    const insertAssetSqlIntoCurrentContext = () => {
-        const sql = `SELECT\n  *\nFROM\n  ${asset.name}\nLIMIT 1000`;
-        setInsertCurrentSqlContent({ sql, title: asset.name, resourceId: asset.resource_id });
-    }
-
-    return (
-        <div className='w-full flex flex-col h-full'>
-            <div className='flex justify-between items-center text-center bg-muted border-y'>
-                {context === 'NOTEBOOK' && (
-                    <div className='flex justify-between w-full'>
-                        <Button onClick={() => clearAsset()} className='gap-1 flex items-center' size='sm' variant='ghost'>
-                            <X className='w-4 h-4' />
-                        </Button>
-                        <Button onClick={insertAssetSqlIntoCurrentContext} className='gap-1 flex items-center' size='sm' variant='ghost'>
-                            <div>Open in Notebook</div>
-                            <SquareArrowOutUpRight className='w-4 h-4' />
-                        </Button>
+  return (
+    <div className="w-full flex flex-col h-full">
+      <div className="flex justify-between items-center text-center bg-muted border-y">
+        {context === "NOTEBOOK" && (
+          <div className="flex justify-between w-full">
+            <Button
+              onClick={() => clearAsset()}
+              className="gap-1 flex items-center"
+              size="sm"
+              variant="ghost"
+            >
+              <X className="w-4 h-4" />
+            </Button>
+            <Button
+              onClick={insertAssetSqlIntoCurrentContext}
+              className="gap-1 flex items-center"
+              size="sm"
+              variant="ghost"
+            >
+              <div>Open in Notebook</div>
+              <SquareArrowOutUpRight className="w-4 h-4" />
+            </Button>
+          </div>
+        )}
+      </div>
+      <div className="flex flex-col h-full mt-4">
+        <ScrollArea className="">
+          <div className="border-none space-y-4 text-sm text-wrap">
+            <div>
+              <div className="border-none px-4 no-underline hover:no-underline bg-muted py-2 font-semibold text-sm">
+                Model Details
+              </div>
+              <div className="border-none p-4 space-y-4">
+                <div>
+                  <div>Model Name</div>
+                  <div className="font-medium text-gray-700">{asset.name}</div>
+                </div>
+                <div>
+                  <div>Dataset</div>
+                  <div className="font-medium text-gray-700">
+                    {asset.dataset}
+                  </div>
+                </div>
+                <div>
+                  <div>Schema</div>
+                  <div className="font-medium text-gray-700">
+                    {asset.schema}
+                  </div>
+                </div>
+                <div>
+                  <div>Table</div>
+                  <div className="font-medium text-gray-700">
+                    {asset.table_name}
+                  </div>
+                </div>
+                <div>
+                  <div>Description</div>
+                  {isUsingAiDescription && <Magic className="w-4 h-4" />}
+                  {assetDescription ? (
+                    <div className="font-medium">{assetDescription}</div>
+                  ) : (
+                    <div className="italic text-muted-foreground opacity-60">
+                      No description
                     </div>
+                  )}
+                </div>
+                <div>
+                  <div className="font-bold">URL</div>
+                  {asset.config?.url ? (
+                    <a
+                      href={asset.config?.url}
+                      target="_blank"
+                      className="text-blue-500 lowercase"
+                    >
+                      {asset.config?.url}
+                    </a>
+                  ) : (
+                    <div className="italic text-muted-foreground opacity-60">
+                      No url
+                    </div>
+                  )}
+                </div>
+                {asset.tags && (
+                  <div>
+                    <div>Tags</div>
+                    <div className="font-normal grid space-y-2 mt-1 ml-[-8px]">
+                      {asset.tags.map((test: string, i: number) => (
+                        <Badge
+                          variant="secondary"
+                          className="w-fit text-muted-foreground"
+                          key={i}
+                        >
+                          {test}
+                        </Badge>
+                      ))}
+                    </div>
+                  </div>
                 )}
-            </div>
-            <div className='flex flex-col h-full mt-4'>
-                <ScrollArea className=''>
-                    <div className='border-none space-y-4 text-sm text-wrap'>
-                        <div >
-                            <div className='border-none px-4 no-underline hover:no-underline bg-muted py-2 font-semibold text-sm'>
-                                Model Details
-                            </div>
-                            <div className='border-none p-4 space-y-4'>
-                                <div>
-                                    <div>
-                                        Model Name
-                                    </div>
-                                    <div className='font-medium text-gray-700'>{asset.name}</div>
-                                </div>
-                                <div>
-                                    <div>
-                                        Dataset
-                                    </div>
-                                    <div className='font-medium text-gray-700'>{asset.dataset}</div>
-                                </div>
-                                <div>
-                                    <div>
-                                        Schema
-                                    </div>
-                                    <div className='font-medium text-gray-700'>{asset.schema}</div>
-                                </div>
-                                <div>
-                                    <div>
-                                        Table
-                                    </div>
-                                    <div className='font-medium text-gray-700'>{asset.table_name}</div>
-                                </div>
-                                <div>
-                                    <div>
-                                        Description
-                                    </div>
-                                    {asset.description ? (
-                                        <div className='font-medium'>{asset.description}</div>
-
-                                    ) : (
-                                        <div className='italic text-muted-foreground opacity-60'>No description</div>
-                                    )}
-                                </div>
-                                <div>
-                                    <div className='font-bold'>
-                                        URL
-                                    </div>
-                                    {asset.config?.url ? (
-                                        <a href={asset.config?.url} target="_blank" className="text-blue-500 lowercase">
-                                            {asset.config?.url}
-                                        </a>
-
-                                    ) : (
-                                        <div className='italic text-muted-foreground opacity-60'>No url</div>
-                                    )}
-                                </div>
-                                {asset.tags && (
-
-
-                                    <div>
-                                        <div>
-                                            Tags
-                                        </div>
-                                        <div className='font-normal grid space-y-2 mt-1 ml-[-8px]'>
-                                            {asset.tags.map((test: string, i: number) => (
-                                                <Badge variant='secondary' className='w-fit text-muted-foreground' key={i}>{test}</Badge>
-                                            ))}
-                                        </div>
-                                    </div>
-                                )}
-                                <div>
-                                    <div>
-                                        Materialization
-                                    </div>
-                                    <div className='font-mono font-medium'>{asset.materialization}</div>
-                                </div>
-                                <div>
-                                    <div>
-                                        Tests
-                                    </div>
-                                    {asset.tests ? (
-                                        <div className='font-normal grid space-y-2 mt-1 ml-[-8px]'>
-                                            {asset.tests.map((test: string, i: number) => (
-                                                <Badge variant='secondary' className='w-fit text-muted-foreground' key={i}>{test}</Badge>
-                                            ))}
-                                        </div>
-                                    ) : (
-                                        <div className='italic text-muted-foreground opacity-60'>No tests</div>
-                                    )}
-
-                                </div>
-                            </div>
-                        </div>
-                        {asset.columns && (
-                            <div>
-                                <div className='border-none px-4 no-underline hover:no-underline bg-muted py-2 font-semibold text-sm'>
-                                    {`Columns(${asset.columns.length})`}
-                                </div>
-                                <div className='space-y-2'>
-                                    {asset.columns.map((column: any, i: number) => (
-                                        <Fragment key={i}>
-                                            <div className='p-4'>
-                                                <div className=''>
-                                                    <div className='font-bold font-mono'>{column.name}</div>
-                                                    <div className='font-mono font-medium text-gray-400 flex items-center text-xs'><ColumnTypeIcon dataType={column.type} className="size-4 mr-1" />{column.type}</div>
-                                                    <div className='font-normal grid gric- space-y-2 ml-[-8px]'>
-                                                        {(column.tests || []).map((test: string, i: number) => (
-                                                            <Badge variant='secondary' className='w-fit text-muted-foreground' key={i}>{test}</Badge>
-                                                        ))}
-                                                    </div>
-                                                    <div className='pt-1 font-normal'>{column.description}</div>
-                                                </div>
-                                            </div>
-                                            <Separator className='text-black' />
-
-                                        </Fragment>
-
-                                    ))}
-                                </div>
-                            </div>
-                        )}
+                <div>
+                  <div>Materialization</div>
+                  <div className="font-mono font-medium">
+                    {asset.materialization}
+                  </div>
+                </div>
+                <div>
+                  <div>Tests</div>
+                  {asset.tests ? (
+                    <div className="font-normal grid space-y-2 mt-1 ml-[-8px]">
+                      {asset.tests.map((test: string, i: number) => (
+                        <Badge
+                          variant="secondary"
+                          className="w-fit text-muted-foreground"
+                          key={i}
+                        >
+                          {test}
+                        </Badge>
+                      ))}
                     </div>
-                    <div className="h-24" />
-                </ScrollArea>
+                  ) : (
+                    <div className="italic text-muted-foreground opacity-60">
+                      No tests
+                    </div>
+                  )}
+                </div>
+              </div>
             </div>
-        </div >
-    )
+            {asset.columns && (
+              <div>
+                <div className="border-none px-4 no-underline hover:no-underline bg-muted py-2 font-semibold text-sm">
+                  {`Columns(${asset.columns.length})`}
+                </div>
+                <div className="space-y-2">
+                  {asset.columns.map((column: any, i: number) => (
+                    <ColumnPreviewer key={column.id} column={column} />
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+          <div className="h-24" />
+        </ScrollArea>
+      </div>
+    </div>
+  );
 }
