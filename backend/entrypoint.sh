edb8f5e8c1599436fbc684af4941652ff022853e
turntable-justin <122627527+turntable-justin@users.noreply.github.com>
2024-08-27 14:15:31 -0500

    fix: fix entrypoints pt. 2

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,8 +6,6 @@ echo "Apply database migrations"
 python manage.py migrate
 echo "Apply hatchet permissions"
 python manage.py hatchet_permissions
-echo "Seed data"
-python manage.py seed_data
 
 # Start the Django server
 exec "$@"
