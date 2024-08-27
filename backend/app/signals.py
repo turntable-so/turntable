752b6307624bd0166bf45678c9f211a92dd8cd92
turntable-justin <122627527+turntable-justin@users.noreply.github.com>
2024-08-27 08:01:30 -0500

    fix: ensure BI assets have types

--- backend/app/signals.py
+++ backend/app/signals.py
@@ -1,14 +1,17 @@
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from channels.layers import get_channel_layer
-from asgiref.sync import async_to_sync
 
 from app.models import WorkflowRun
 
 
 @receiver(pre_save, sender=WorkflowRun)
 def send_status_update(sender, instance, **kwargs):
-    old_status = WorkflowRun.objects.get(pk=instance.pk).status
+    try:
+        old_status = WorkflowRun.objects.get(pk=instance.pk).status
+    except WorkflowRun.DoesNotExist:
+        old_status = None
     new_status = instance.status
 
     if old_status == new_status:
@@ -16,15 +19,15 @@ def send_status_update(sender, instance, **kwargs):
     workspace_id = instance.resource.workspace_id
     channel_layer = get_channel_layer()
     try:
-      async_to_sync(channel_layer.group_send)(
-          f"workspace_{workspace_id}",
-          {
-              "type": "workflow_status_update",
-              "status": instance.status,
-              "workflow_run_id": str(instance.id),
-              "resource_id": str(instance.resource.id),
-          },
-      )
+        async_to_sync(channel_layer.group_send)(
+            f"workspace_{workspace_id}",
+            {
+                "type": "workflow_status_update",
+                "status": instance.status,
+                "workflow_run_id": str(instance.id),
+                "resource_id": str(instance.resource.id),
+            },
+        )
     except Exception as e:
-      print(e)
-      print("Error sending status update")
+        print(e)
+        print("Error sending status update")
