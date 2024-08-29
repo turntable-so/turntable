e918992b04a0dcfbda338e370b244cc82907120a
Ian Tracey <ianrtracey@gmail.com>
2024-08-29 16:40:09 -0400

    lauch readme and remove font

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -19,10 +19,6 @@ const config = {
       },
     },
     extend: {
-      fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
-        "carter-one": ["Carter One", "serif"],
-      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
