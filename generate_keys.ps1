0e6fa3bf4916758106063b17d13cccc94e15ec76
turntable-justin <122627527+turntable-justin@users.noreply.github.com>
2024-08-29 19:00:29 -0500

    fix: fix windows key generations script

--- generate_keys.ps1
+++ generate_keys.ps1
@@ -1,12 +1,20 @@
 # Generate a random base64 encryption key and append to .env file
-$encryptionKey = [Convert]::ToBase64String((New-Object System.Security.Cryptography.RNGCryptoServiceProvider).GetBytes(32))
-Add-Content -Path ".env" -Value "ENCRYPTION_KEY=""$encryptionKey"""
+$byteArray = New-Object 'System.Byte[]' (32)
+(New-Object System.Security.Cryptography.RNGCryptoServiceProvider).GetBytes($byteArray)
+$encryptionKey = [Convert]::ToBase64String($byteArray)
+Set-Content -Path ".env" -Value "ENCRYPTION_KEY='$encryptionKey'"
 
 # Generate a random base64 nextauth secret and append to .env file
-$nextAuthSecret = [Convert]::ToBase64String((New-Object System.Security.Cryptography.RNGCryptoServiceProvider).GetBytes(32))
-Add-Content -Path ".env" -Value "NEXTAUTH_SECRET=""$nextAuthSecret"""
+$byteArray2 = New-Object 'System.Byte[]' (32)
+(New-Object System.Security.Cryptography.RNGCryptoServiceProvider).GetBytes($byteArray2)
+$nextAuthSecret = [Convert]::ToBase64String($byteArray2)
+Add-Content -Path ".env" -Value "NEXTAUTH_SECRET='$nextAuthSecret'"
 
 # Generate a random 50 character string and append to .env file
-$chars = 'a'..'z' + '0'..'9' + '!@#$%^&*(-_=+)'
-$randomString = -join (1..50 | ForEach-Object { $chars[(Get-Random -Maximum $chars.Length)] })
-Add-Content -Path ".env" -Value $randomString
+$chars = @(
+    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+    '0','1','2','3','4','5','6','7','8','9',
+    '!','@','#','$','%','^','&','*','(',')','-','=','_','+'
+)
+$djangoSecretKey = -join (1..50 | ForEach-Object { $chars[(Get-Random -Minimum 0 -Maximum $chars.Length)] })
+Add-Content -Path ".env" -Value "DJANGO_SECRET_KEY='$djangoSecretKey'"
