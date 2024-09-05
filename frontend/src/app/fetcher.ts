adf68a9c6da3b0648e3fa2ae0578fabeed267222
Cristhian <gfcristhian98@gmail.com>
2024-09-05 12:39:50 -0500

    fix: fix api url

--- frontend/src/app/fetcher.ts
+++ frontend/src/app/fetcher.ts
@@ -59,7 +59,6 @@ export const fetcher = (
         ...(cookies ? { cookies } : {}),
         signal,
     };
-    console.log("body", body)
     if (!(body instanceof FormData)) {
         fetchOptions['headers'] = {
             "Content-Type": "application/json",
--- frontend/src/app/url.ts
+++ frontend/src/app/url.ts
@@ -1,3 +1,3 @@
 export default function getUrl() {
-    return process.env.NEXT_PUBLIC_BACKEND_HOST || (typeof window === "undefined" ? "http://localhost:8000" : "http://localhost:8000")
+    return process.env.NEXT_PUBLIC_BACKEND_HOST || (typeof window === "undefined" ? "http://api:8000" : "http://localhost:8000")
 } 
