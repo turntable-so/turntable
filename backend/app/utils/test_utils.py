b9395cefacbf65562b581f4a056e4a6452e3b003
turntable-justin <122627527+turntable-justin@users.noreply.github.com>
2024-09-06 16:21:13 -0400

    feature: add databricks connection backend

---
+++ backend/app/utils/test_utils.py
@@ -0,0 +1,75 @@
+import os
+from functools import wraps
+
+import pytest
+
+from app.models import Asset, AssetLink, Column, ColumnLink, ResourceType
+
+
+def require_env_vars(*required_vars):
+    """
+    Decorator to skip a test if the specified environment variables are not set.
+
+    :param required_vars: Names of required environment variables
+    """
+
+    def decorator(test_func):
+        @wraps(test_func)
+        def wrapper(*args, **kwargs):
+            missing_vars = [var for var in required_vars if var not in os.environ]
+            if missing_vars:
+                pytest.skip(
+                    f"Skipping test because environment variables are missing: {', '.join(missing_vars)}"
+                )
+            return test_func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+def assert_ingest_output(resources):
+    ## all tables have records
+    assert Asset.objects.count() > 0
+    assert AssetLink.objects.count() > 0
+    assert Column.objects.count() > 0
+    assert ColumnLink.objects.count() > 0
+
+    ## all assets have types
+    for asset in Asset.objects.all():
+        assert asset.type is not None, asset.__dict__
+
+    ## all resources represented in assets
+    for resource in resources:
+        assert resource.asset_set.count() > 0
+
+    ## at least one asset link across resources if multiple resources used
+    if len(resources) > 1:
+        links_across_resources = [
+            v.id
+            for v in AssetLink.objects.all()
+            if v.source.resource.id != v.target.resource.id
+        ]
+        assert len(links_across_resources) > 0
+
+    ## all connected db assets have a column link
+    db_assets = Asset.objects.filter(resource__type=ResourceType.DB).values_list(
+        "id", flat=True
+    )
+    db_assets_with_asset_links = set()
+    for al in AssetLink.objects.all():
+        for id in [al.source.id, al.target.id]:
+            if id in db_assets:
+                db_assets_with_asset_links.add(id)
+    db_assets_with_column_links = set()
+    for cl in ColumnLink.objects.all():
+        for id in [cl.source.asset.id, cl.target.asset.id]:
+            if id in db_assets:
+                db_assets_with_column_links.add(id)
+    assert db_assets_with_asset_links == db_assets_with_column_links
+
+    ## all instances have workspace_ids
+    assert Asset.objects.filter(workspace_id=None).count() == 0
+    assert AssetLink.objects.filter(workspace_id=None).count() == 0
+    assert Column.objects.filter(workspace_id=None).count() == 0
+    assert ColumnLink.objects.filter(workspace_id=None).count() == 0
--- backend/tests/test_resource_connections.py
+++ backend/tests/test_resource_connections.py
@@ -1,6 +1,7 @@
 import pytest
 
 from app.models import Resource
+from app.utils.test_utils import require_env_vars
 
 
 @pytest.mark.django_db
@@ -15,3 +16,13 @@ def test_datahub_connection(local_postgres: Resource, local_metabase: Resource):
 def test_db_connection(local_postgres: Resource):
     lp = local_postgres.details.test_db_connection()
     assert lp["success"], lp
+
+
+@pytest.mark.django_db
+@require_env_vars("DATABRICKS_1_WORKSPACE_ID")
+def test_databricks_connection(remote_databricks: Resource):
+    dh = remote_databricks.details.test_datahub_connection()
+    assert dh["success"], dh
+
+    db = remote_databricks.details.test_db_connection()
+    assert db["success"], db
