196512abf5b261f2975e3a41d3014c1e35bcf086
jacobiajohnson <jacobia@workos.com>
2024-09-07 10:21:37 -0400

    Remove legacy secrets UI in settings page

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -1,19 +1,8 @@
 import { Suspense } from "react";
 import FullWidthPageLayout from "../../components/layout/FullWidthPageLayout";
-import AuthenticationProfiles from "./AuthenticationProfiles";
-
 
 export default async function SettingsPage() {
+  const authProfiles = [] as any;
 
-    const authProfiles = [] as any
-
-    return (
-        <FullWidthPageLayout title='Settings'>
-            <div className='w-full py-4 rounded-lg space-y-16' >
-                <Suspense>
-                    <AuthenticationProfiles authProfiles={authProfiles || []} />
-                </Suspense>
-            </div>
-        </FullWidthPageLayout>
-    )
-} 
+  return <FullWidthPageLayout title="Settings"></FullWidthPageLayout>;
+}
