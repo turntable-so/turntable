841913e64831f0963864172f1d5a9319f6b674a5
jacobiajohnson <jacobia@workos.com>
2024-09-07 10:25:43 -0400

    Clean up unused variable

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -2,7 +2,5 @@ import { Suspense } from "react";
 import FullWidthPageLayout from "../../components/layout/FullWidthPageLayout";
 
 export default async function SettingsPage() {
-  const authProfiles = [] as any;
-
   return <FullWidthPageLayout title="Settings"></FullWidthPageLayout>;
 }
