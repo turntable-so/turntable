d7c225880dbd50618e8ddc0f48239a289d0df5df
turntable-justin <122627527+turntable-justin@users.noreply.github.com>
2024-09-09 16:02:01 -0500

    fix: use .env.local instead of sourcing approach. More reliable.

--- generate_keys.sh
+++ generate_keys.sh
@@ -2,13 +2,13 @@
 platform=$(uname)
 
 if [[ "$platform" == "Darwin" ]]; then
-    echo "ENCRYPTION_KEY=\"`openssl rand -base64 32`\"" >> .env
-    echo "NEXTAUTH_SECRET=\"`openssl rand -base64 32`\"" >> .env
-    echo "DJANGO_SECRET_KEY=\"`env LC_ALL=C tr -dc 'a-z0-9!@#$%^&*(-_=+)' < /dev/urandom | head -c 50`\"" >> .env
+    echo "ENCRYPTION_KEY=\"`openssl rand -base64 32`\"" >> .env.local
+    echo "NEXTAUTH_SECRET=\"`openssl rand -base64 32`\"" >> .env.local
+    echo "DJANGO_SECRET_KEY=\"`env LC_ALL=C tr -dc 'a-z0-9!@#$%^&*(-_=+)' < /dev/urandom | head -c 50`\"" >> .env.local
 elif [[ "$platform" == "Linux" ]]; then
-    echo "ENCRYPTION_KEY=\"`openssl rand -base64 32`\"" >> .env
-    echo "NEXTAUTH_SECRET=\"`openssl rand -base64 32`\"" >> .env
-    echo "DJANGO_SECRET_KEY=\"`< /dev/urandom tr -dc 'a-z0-9!@#$%^&*(-_=+)' | head -c 50`\"" >> .env
+    echo "ENCRYPTION_KEY=\"`openssl rand -base64 32`\"" >> .env.local
+    echo "NEXTAUTH_SECRET=\"`openssl rand -base64 32`\"" >> .env.local
+    echo "DJANGO_SECRET_KEY=\"`< /dev/urandom tr -dc 'a-z0-9!@#$%^&*(-_=+)' | head -c 50`\"" >> .env.local
 else
     echo "Unsupported platform: $platform"
 fi
