0036607e3697f832aefbdefa202ca1f39b9e235f
turntable-justin <122627527+turntable-justin@users.noreply.github.com>
2024-09-08 20:52:43 -0400

    fix: fix broken tests

--- backend/app/services/tests/test_lineage_service.py
+++ backend/app/services/tests/test_lineage_service.py
@@ -5,9 +5,7 @@ from app.services.lineage_service import LineageService
 
 @pytest.mark.django_db
 def test_get_asset_lineage(prepopulated_dev_db):
-    asset_id = (
-        "urn:li:dataset:(urn:li:dataPlatform:postgres,mydb.dbt_sl_test.raw_items,PROD)"
-    )
+    asset_id = "0:urn:li:dataset:(urn:li:dataPlatform:postgres,mydb.dbt_sl_test.raw_items,PROD)"
     out = LineageService(prepopulated_dev_db[0].workspace.id).get_lineage(
         asset_id, 2, 2
     )
