d0e7855b7216cefdc08437529ed5adbf502d28e9
turntable-justin <122627527+turntable-justin@users.noreply.github.com>
2024-09-12 09:52:55 -0400

    fix: query test

--- backend/workflows/tests/test_query.py
+++ backend/workflows/tests/test_query.py
@@ -1,5 +1,6 @@
 import pytest
 import requests
+from django.conf import settings
 
 from app.models import Resource
 from app.utils.test_utils import require_env_vars
@@ -16,14 +17,17 @@ def run_test_query(resource: Resource, user, query=TEST_QUERY, output_len=10):
         "query": query,
     }
     result = WorkflowDebugger(ExecuteQueryWorkflow, input).run().result()
-    url = result["execute_query"]["signed_url"].replace("localhost", "minio")
+    url = result["execute_query"]["signed_url"].replace(
+        settings.AWS_S3_PUBLIC_URL, settings.AWS_S3_ENDPOINT_URL
+    )
+
     data = requests.get(url)
     assert data.status_code == 200
     assert len(data.json()["data"]) == output_len
 
 
 @pytest.mark.django_db
-def test_query(local_postgres, user):
+def test_query_local(local_postgres, user):
     run_test_query(local_postgres, user)
 
 
