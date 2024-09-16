1d4d28852ed719946e8fce0dd885eca0b8835913
Ian Tracey <ianrtracey@gmail.com>
2024-09-16 11:33:54 -0400

    ui asset row schema change

--- frontend/src/components/ui/schema.ts
+++ frontend/src/components/ui/schema.ts
@@ -11,7 +11,9 @@ export const assetSchema = z.object({
     tags: z.array(z.string()),
     num_columns: z.number(),
     resource_subtype: z.string(),
-    resource_has_dbt: z.boolean()
+    resource_has_dbt: z.boolean(),
+    resource_id: z.string(),
+    resource_name: z.string(),
 })
 
 export type Asset = z.infer<typeof assetSchema>
