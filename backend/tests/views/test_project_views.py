5a3fabe1a5c8fea49ab8b6aac440a538c1d7ee62
turntable-justin <122627527+turntable-justin@users.noreply.github.com>
2024-10-10 18:46:19 -0500

    fix lineage issues

--- backend/tests/views/test_project_views.py
+++ backend/tests/views/test_project_views.py
@@ -97,7 +97,15 @@ class TestProjectViews:
 
         assert response.status_code == 204
 
-    def test_get_lineage_view(self, client, encoded_filepath):
+    @pytest.mark.parametrize(
+        "filepath",
+        [
+            "models/marts/customer360/customers.sql",
+            "models/staging/stg_products.sql",
+        ],
+    )
+    def test_get_lineage_view(self, client, filepath):
+        encoded_filepath = safe_encode(filepath)
         response = client.get(
             "/project/lineage/",
             {
