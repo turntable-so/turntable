f0a6fc7e51846a7581cc4b2ab18071c282036319
Sami <sami@turntable.so>
2024-10-31 11:29:00 -0400

    fix frontend api url (#159)

--- frontend/src/lib/feature-flags.tsx
+++
@@ -1,8 +0,0 @@
-type FeatureFlags = {
-  notebookTabExp: boolean;
-};
-export default function getFeatureFlags(tenantId: string): FeatureFlags {
-  return {
-    notebookTabExp: tenantId === "0",
-  };
-}
