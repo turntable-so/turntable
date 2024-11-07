ee35750823b52d971f454f60acaf6236af7f23a5
Matt Kaye <mrkaye97@gmail.com>
2024-11-07 16:17:38 -0500

    task for generating descriptions for models

--- backend/app/workflows/metadata.py
+++ backend/app/workflows/metadata.py
@@ -3,7 +3,8 @@ import tempfile
 from celery import shared_task
 
 from app.core.e2e import DataHubDBParser
-from app.models import Resource, ResourceSubtype
+from app.models import Resource, ResourceSubtype, Asset, DBTResource, Branch, Column
+from ai.documentation.dbt import create_model_description
 
 
 @shared_task
@@ -41,8 +42,47 @@ def process_metadata(workspace_id: str, resource_id: str):
     DataHubDBParser.combine_and_upload([parser], resource)
 
 
+
+
+
+@shared_task
+def create_model_descriptions(workspace_id: str, resource_id: str) -> list[str]:
+    models = (
+        Asset.objects.filter(
+            workspace_id=workspace_id,
+            resource_id=resource_id,
+            type=Asset.AssetType.MODEL
+        )
+    )
+
+    import os
+    os.getenv("OPENAI_API_KEY")
+
+    for model in models:
+        print()
+        model_name = model.name
+        sql = model.sql
+
+        columns = (
+            Column.objects.filter(
+                asset_id=model.id
+            )
+        )
+
+        cols = [column.name + " " + column.type for column in columns]
+
+        description = create_model_description(
+            model_name=model_name,
+            schema=cols,
+            compiled_sql=sql,
+        )
+
+        print(description)
+
+
 @shared_task(bind=True)
 def sync_metadata(self, workspace_id: str, resource_id: str):
+    print("\n\nRunning Sync metadata\n\n")
     prepare_dbt_repos(workspace_id=workspace_id, resource_id=resource_id)
     ingest_metadata(
         workspace_id=workspace_id,
@@ -50,4 +90,5 @@ def sync_metadata(self, workspace_id: str, resource_id: str):
         workunits=1000,
         task_id=self.request.id,
     )
+    create_model_descriptions(workspace_id=workspace_id, resource_id=resource_id)
     process_metadata(workspace_id=workspace_id, resource_id=resource_id)
