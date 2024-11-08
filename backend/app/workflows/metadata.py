e550ca0a8f8371c147a47bb03f9d1c83d0c1b9a4
Matt Kaye <mrkaye97@gmail.com>
2024-11-08 11:46:06 -0500

    batch generate column descriptions

--- backend/app/workflows/metadata.py
+++ backend/app/workflows/metadata.py
@@ -1,11 +1,14 @@
 import tempfile
+from typing import TypeVar, Generator
 
 from celery import shared_task
+from pydantic import BaseModel
 
 from app.core.e2e import DataHubDBParser
-from app.models import Resource, ResourceSubtype, Asset, DBTResource, Branch, Column
-from ai.documentation.dbt import create_model_description
+from app.models import Resource, ResourceSubtype, Asset, Column
+from ai.documentation.dbt import generate_model_description, generate_column_descriptions, ColumnDescriptionGenInputs
 
+T = TypeVar("T")
 
 @shared_task
 def prepare_dbt_repos(workspace_id: str, resource_id: str):
@@ -46,7 +49,7 @@ def process_metadata(workspace_id: str, resource_id: str):
 
 @shared_task
 def create_single_model_description(workspace_id: str, model_id: str, model_name: str, schema: str, compiled_sql: str) -> None:
-    description = create_model_description(
+    description = generate_model_description(
         model_name=model_name,
         schema=schema,
         compiled_sql=compiled_sql,
@@ -55,6 +58,66 @@ def create_single_model_description(workspace_id: str, model_id: str, model_name
     model = Asset.objects.get(id=model_id)
     model.ai_description = description.description
     model.save()
+
+@shared_task
+def batch_create_column_descriptions(workspace_id: str, columns: list[ColumnDescriptionGenInputs]) -> None:
+    descriptions = generate_column_descriptions(
+        columns=columns,
+    )
+
+    for column in columns:
+        generated_description = next(filter(lambda x: x.column_id == column.column_id, descriptions), None)
+
+        if not generated_description:
+            continue
+
+        db_column = Column.objects.get(id=column.column_id)
+
+        db_column.ai_description = generated_description
+        db_column.save()
+
+@shared_task
+def create_column_descriptions(workspace_id: str, resource_id: str) -> list[str]:
+    def chunk(xs: list[T], n: int) -> Generator[list[T], None, None]:
+        for i in range(0, len(xs), n):
+            yield xs[i:i + n]
+
+    models = (
+        Asset.objects.filter(
+            workspace_id=workspace_id,
+            resource_id=resource_id,
+            type=Asset.AssetType.MODEL
+        )
+    )
+
+    for model in models:
+        column_chunks = chunk(
+            xs=Column.objects.filter(
+                asset_id=model.id
+            ),
+            n=20
+        )
+
+        for column_chunk in column_chunks:
+            ## IMPORTANT: Performance note - This will be a big bottleneck.
+            ## If we don't run Celery with high concurrency and these requests take
+            ## multiple seconds each, this fanout will cause the tasks in the broker to pile
+            ## up. Running many threads with `--pool gevent` or `--pool eventlet` can help.
+            batch_create_column_descriptions.delay(
+                workspace_id=workspace_id,
+                columns=[
+                    ColumnDescriptionGenInputs(
+                        column_id=column.id,
+                        model_name=model.name,
+                        column_name=column.name,
+                        column_type=column.type,
+                        schema="\n".join([column.name + " " + column.type for column in column_chunk]),
+                        compiled_sql=model.sql,
+                    )
+                    for column in column_chunk
+                ]
+            )
+
 
 @shared_task
 def create_model_descriptions(workspace_id: str, resource_id: str) -> list[str]:
@@ -96,4 +159,5 @@ def sync_metadata(self, workspace_id: str, resource_id: str):
         task_id=self.request.id,
     )
     create_model_descriptions(workspace_id=workspace_id, resource_id=resource_id)
+    create_column_descriptions(workspace_id=workspace_id, resource_id=resource_id)
     process_metadata(workspace_id=workspace_id, resource_id=resource_id)
