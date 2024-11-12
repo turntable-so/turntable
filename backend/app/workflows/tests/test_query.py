ed89d7fc97b5004d7eec0bcceb733b4612b688f2
turntable-justin <122627527+turntable-justin@users.noreply.github.com>
2024-11-12 08:14:28 -0600

    fix test queries

--- backend/app/workflows/tests/test_query.py
+++ backend/app/workflows/tests/test_query.py
@@ -6,9 +6,9 @@ from app.models import Resource
 from app.utils.test_utils import require_env_vars
 from app.workflows.query import execute_dbt_query, execute_query
 
-TEST_QUERY = "select * from mydb.dev.orders"
+TEST_QUERY = "select * from mydb.dbt_sl_test.raw_products"
 
-TEST_DBT_QUERY = "select * from {{ ref('orders') }}"
+TEST_DBT_QUERY = "select * from {{ ref('raw_products') }}"
 
 
 def run_test_query(
