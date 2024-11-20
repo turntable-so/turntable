46f31e60c853d653c1414d6560203c79621fc5fa
turntable-justin <122627527+turntable-justin@users.noreply.github.com>
2024-11-20 12:33:54 -0600

    WIP

--- backend/app/workflows/utils.py
+++ backend/app/workflows/utils.py
@@ -6,6 +6,8 @@ from celery import Task, shared_task, states
 from celery import chain as celery_chain
 from django_celery_results.models import TaskResult
 
+from app.utils.obj import make_values_serializable
+
 
 class CustomTask(Task):
     """
@@ -13,29 +15,45 @@ class CustomTask(Task):
     """
 
     def apply(self, *args, **kwargs):
+        if os.getenv("CUSTOM_CELERY_EAGER") != "true":
+            return super().apply(*args, **kwargs)
+
         kwargs["task_id"] = kwargs.get("task_id") or str(uuid.uuid4())
         task_id = kwargs["task_id"]
+        task_args_json = (
+            json.dumps(make_values_serializable(args[0])) if args[0] else None
+        )
+        task_kwargs_json = (
+            json.dumps(make_values_serializable(args[1])) if args[1] else None
+        )
 
         tr = TaskResult.objects.create(
             task_id=task_id,
             status=states.PENDING,
-            task_args=json.dumps(args[0]) if args[0] else None,
-            task_kwargs=json.dumps(args[1]) if args[1] else None,
+            task_args=task_args_json,
+            task_kwargs=task_kwargs_json,
         )
-        res = Task.apply(self, *args, **kwargs)
-        tr.result = res.get()
-        tr.status = res.status
-        tr.save()
-        return res
+        try:
+            print(args[0], args[1])
+            res = self(*args[0], **args[1])
+            tr.result = res.get()
+            tr.status = res.status
+            tr.save()
+            return res
+        except Exception as e:
+            tr.status = states.FAILURE
+            tr.save()
+            raise e
 
     def apply_async(self, *args, **kwargs):
+        if os.getenv("CUSTOM_CELERY_EAGER") != "true":
+            return super().apply_async(*args, **kwargs)
         return self.apply(*args, **kwargs)
 
 
 def task(*args, **kwargs):
     kwargs.setdefault("bind", True)
-    if os.getenv("CUSTOM_CELERY_EAGER") == "true":
-        kwargs.setdefault("base", CustomTask)
+    kwargs.setdefault("base", CustomTask)
     return shared_task(*args, **kwargs)
 
 
--- backend/conftest.py
+++ backend/conftest.py
@@ -38,6 +38,9 @@ def pytest_addoption(parser):
     parser.addoption(
         "--use_cache", action="store_true", default=False, help="Use cached fixtures"
     )
+    parser.addoption(
+        "--eager", action="store_true", default=False, help="Run tasks eagerly"
+    )
 
 
 def pytest_configure(config):
@@ -67,6 +70,24 @@ def pytest_collection_modifyitems(config, items):
         for item in items:
             if "internal" in item.keywords:
                 item.add_marker(skip_internal)
+
+
+@pytest.fixture
+def eager(request):
+    if request.config.getoption("--eager"):
+        os.environ["CUSTOM_CELERY_EAGER"] = "true"
+        return True
+    return False
+
+
+@pytest.fixture
+def recache(request):
+    return request.config.getoption("--recache")
+
+
+@pytest.fixture
+def use_cache(request):
+    return request.config.getoption("--use_cache")
 
 
 @pytest.fixture()
@@ -169,16 +190,6 @@ def prepopulated_dev_db(local_metabase, local_postgres):
 
 
 @pytest.fixture
-def recache(request):
-    return request.config.getoption("--recache")
-
-
-@pytest.fixture
-def use_cache(request):
-    return request.config.getoption("--use_cache")
-
-
-@pytest.fixture
 def force_isolate(monkeypatch):
     monkeypatch.setenv("FORCE_ISOLATE", "true")
 
@@ -217,7 +228,9 @@ def test_queue_name():
 
 
 @pytest.fixture(scope="session")
-def custom_celery_app(test_queue_name):
+def custom_celery_app(test_queue_name, eager):
+    if eager:
+        return None
     app = Celery("api")
 
     # Load configuration from Django settings
@@ -234,8 +247,12 @@ def custom_celery_app(test_queue_name):
 def custom_celery_worker(
     custom_celery_app,
     test_queue_name,
+    eager,
     max_retries: int = 10,
 ):
+    if eager:
+        return
+
     # Clear the queue before starting the worker
     custom_celery_app.control.purge()
 
@@ -265,11 +282,14 @@ def custom_celery_worker(
 
 
 @pytest.fixture(scope="session")
-def suppress_celery_errors():
+def suppress_celery_errors(eager):
     """
     Suppress error logs from celery.worker.control and kombu.pidbox
     by setting their log levels to CRITICAL.
     """
+    if eager:
+        return
+
     loggers_to_suppress = [
         "celery.worker.control",
         "kombu.pidbox",
@@ -292,4 +312,6 @@ def suppress_celery_errors():
 
 @pytest.fixture(scope="session")
 def custom_celery(custom_celery_worker, bypass_celery_beat, suppress_celery_errors):
+    if eager:
+        return None
     return custom_celery_worker
