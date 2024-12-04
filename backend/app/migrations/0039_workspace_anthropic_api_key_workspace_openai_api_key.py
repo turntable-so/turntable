a22029a6c9f4fa4cf96a909e893e2da81025a11f
Sami <sami@turntable.so>
2024-12-03 16:50:04 -0800

    fix migrations

--- backend/app/migrations/0039_workspace_anthropic_api_key_workspace_openai_api_key.py
+++ backend/app/migrations/0039_workspace_anthropic_api_key_workspace_openai_api_key.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("app", "0037_dbtorchestrator_name"),
+        ("app", "0038_alter_scheduledworkflow_clocked_and_more"),
     ]
 
     operations = [
