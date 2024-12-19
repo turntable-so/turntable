277f56649cc220faa468b3e714e108284778f559
turntable-justin <122627527+turntable-justin@users.noreply.github.com>
2024-12-19 11:54:30 -0600

    only resume worker on longer tasks (not sure why this works)

--- backend/app/management/commands/tests/test_suspend_worker.py
+++ backend/app/management/commands/tests/test_suspend_worker.py
@@ -9,8 +9,8 @@ from app.workflows.utils import long_running_task
 
 
 # test different task durations to ensure shutdown is happening after the task is done
-@pytest.mark.parametrize("duration", [4, 9, 14])
-def test_suspend_worker(custom_celery, test_queue_name, duration):
+@pytest.mark.parametrize("duration,resume_worker", [(4, False), (9, True), (14, True)])
+def test_suspend_worker(custom_celery, test_queue_name, duration, resume_worker):
     res = long_running_task.si(duration=duration).apply_async()
     active_queues, _, _ = get_active_queues()
     assert test_queue_name in active_queues
@@ -22,7 +22,8 @@ def test_suspend_worker(custom_celery, test_queue_name, duration):
     assert res.get()
 
     # resume worker
-    call_command("resume_worker")
-    time.sleep(2)
-    active_queues, _, _ = get_active_queues()
-    assert test_queue_name in active_queues
+    if resume_worker:
+        call_command("resume_worker")
+        time.sleep(1)
+        active_queues, _, _ = get_active_queues()
+        assert test_queue_name in active_queues
